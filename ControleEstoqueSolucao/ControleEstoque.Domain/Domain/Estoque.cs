2f0fcde4fc5954f55dab719e9f224d84d060ef06
Glautter Glauber Ferreira Neves <provider.gglauber@terceiros.mongeralaegon.com.br>
2021-10-17 12:14:23 -0300

    cria classe estoque

---
+++ ControleEstoqueSolucao/ControleEstoque.Domain/Domain/Estoque.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControleEstoque.Domain.Domain
+{
+    public class Estoque
+    {
+        public int Quantidade { get; set; }
+    }
+}
